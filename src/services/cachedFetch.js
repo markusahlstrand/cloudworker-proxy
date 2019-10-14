c2dc06c0f24811742ba59f5a3e1638ee5d68540d
Markus Ahlstrand <markus@ahlstrand.es>
2019-10-14 10:20:28 +0200

    Fix typo

--- src/services/cachedFetch.js
+++ src/services/cachedFetch.js
@@ -2,16 +2,16 @@
 const cache = caches.default;
 
 async function cachedFetch(request) {
-  const cachedJwk = await cache.match(request);
+  const cachedResponse = await cache.match(request);
 
-  if (cachedJwk) {
-    return cachedJwk;
+  if (cachedResponse) {
+    return cachedResponse;
   }
 
   // eslint-disable-next-line no-undef
   const response = await fetch(request);
   if (response.status === 200) {
-    await cache.put(jwksUri, response);
+    await cache.put(request, response);
   }
 
   return response;
