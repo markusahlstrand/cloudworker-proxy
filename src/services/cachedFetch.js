e893d237228cb13e3406ba4bcd7fa7aa6ce40c0c
Markus Ahlstrand <markus@ahlstrand.es>
2019-10-14 10:24:16 +0200

    Pass options to cachedFetch

--- src/services/cachedFetch.js
+++ src/services/cachedFetch.js
@@ -1,17 +1,17 @@
 // eslint-disable-next-line no-undef
 const cache = caches.default;
 
-async function cachedFetch(request) {
-  const cachedResponse = await cache.match(request);
+async function cachedFetch(url, options = {}) {
+  const cachedResponse = await cache.match(url);
 
   if (cachedResponse) {
     return cachedResponse;
   }
 
   // eslint-disable-next-line no-undef
-  const response = await fetch(request);
+  const response = await fetch(url, options);
   if (response.status === 200) {
-    await cache.put(request, response);
+    await cache.put(url, response);
   }
 
   return response;
