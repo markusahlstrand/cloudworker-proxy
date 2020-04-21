390a36e46e8c677d85613ce84a76f7b4efc40d16
Markus Ahlstrand <markus@ahlstrand.es>
2020-04-21 15:22:18 +0200

    Replace params in respons

--- src/handlers/response.js
+++ src/handlers/response.js
@@ -1,16 +1,20 @@
+function resolveParams(data, params = {}) {
+  return Object.keys(params).reduce((acc, key) => acc.replace(`{${key}}`, params[key]), data);
+}
+
 module.exports = function responseHandler({ body = '', headers = {}, status = 200 }) {
   return async (ctx) => {
     if (body instanceof Object) {
       ctx.body = JSON.stringify(body);
       ctx.set('Content-Type', 'application/json');
     } else {
-      ctx.body = body;
+      ctx.body = resolveParams(body, ctx.params);
     }
 
     ctx.status = status;
 
     Object.keys(headers).forEach((key) => {
-      ctx.set(key, headers[key]);
+      ctx.set(key, resolveParams(headers[key], ctx.params));
     });
   };
 };
