ef73639190ef1cd62f06e075be7d6f694b1c8e9e
Skynet <robo@taktile.com>
2023-09-10 08:32:32 +0200

    Integration tests that works by using miniflare to locally execute the README example script

---
+++ integration/helloworld.js
@@ -0,0 +1,19 @@
+const Proxy = require('../src/index.js');
+const config = [
+  {
+    handlerName: 'response',
+    options: {
+      body: 'Hello world',
+    },
+  },
+];
+
+const proxy = new Proxy(config);
+
+async function fetchAndApply(event) {
+  return await proxy.resolve(event);
+}
+
+addEventListener('fetch', (event) => {
+  event.respondWith(fetchAndApply(event));
+});
